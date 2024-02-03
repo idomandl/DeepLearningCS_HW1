c930db1fbdc069082486c3210a85fec40972af5b
Omer Shahar <omershac@post.bgu.ac.il>
2024-02-03 22:20:19 +0200

    Fix size not matching hidden bug

--- SGD.py
+++ SGD.py
@@ -2,24 +2,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 class SGD:
-
     def __init__(self, calc_grad, lr=0.01, stop_condition=0.0001):
-        self.lr = lr
-        self.stop_condition = stop_condition
         self.calc_grad = calc_grad
+        self.lr = lr # learning rate
+        self.stop_condition = stop_condition
 
     def run(self, X, Y, Theta):
-        loss_gradients = []
-        g = self.calc_grad(X, Y, Theta)
-        while np.linalg.norm(g) > self.stop_condition:
-            loss_gradients.append(np.linalg.norm(g))
-            Theta = Theta - self.lr * g
-            # get batch from x and y
-            g = self.calc_grad(X, Y, Theta)
-        return Theta, loss_gradients
-
-
-
+        loss_grads = []
+        grad = self.calc_grad(X, Y, Theta)
+        while np.linalg.norm(grad) > self.stop_condition:
+            loss_grads.append(np.linalg.norm(grad))
+            # print(f'{Theta.shape=}, {(self.lr * grad).shape=}')
+            Theta -= self.lr * grad
+            grad = self.calc_grad(X, Y, Theta)
+        return Theta, loss_grads
 
 class CalcGrad:
     def __init__(self):
@@ -36,18 +32,29 @@ class LeastSquaresCG(CalcGrad):
         return X.T @ (X @ Theta - Y)
 
 
-
-
-test_sgd = SGD(LeastSquaresCG(), lr=0.0001, stop_condition=0.0001)
-# open .mat file
 import scipy.io as sio
+
+BATCH_SIZE = 100
+
+# open .mat file
 mat_contents = sio.loadmat('GMMData.mat')
 print(mat_contents.keys())
-X = mat_contents['Ct'][:,:100]
-Y = mat_contents['Yt'][:,:100]
-print(X.shape, Y.shape)
-Theta = np.zeros((X.shape[1], 1))
-theta, loss_grads = test_sgd.run(X, Y, Theta)
+X = mat_contents['Ct'].T
+Y = mat_contents['Yt'].T
+
+# Shuffle the data
+indices = np.random.permutation(X.shape[0])
+X = X[indices][:BATCH_SIZE]
+Y = Y[indices][:BATCH_SIZE]
+
+# X: (batch_size, n), Y: (batch_size, m), Theta: (n, m)
+Theta = np.zeros((X.shape[1], Y.shape[1]))
+print(f'{X.shape=}, {Y.shape=}, {Theta.shape=}')
+
+# train
+my_sgd = SGD(LeastSquaresCG(), lr=0.0001, stop_condition=0.0001)
+theta, loss_grads = my_sgd.run(X, Y, Theta)
 print(theta)
+print(f'{theta.shape=}')
 plt.plot(loss_grads)
 plt.show()
