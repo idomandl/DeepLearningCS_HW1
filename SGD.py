15d3a45b22846e46d4c979690b8ea1db5e112ea9
Omer Shahar <omershac@post.bgu.ac.il>
2024-02-04 03:01:48 +0200

    Add batches and epochs to SGD

--- SGD.py
+++ SGD.py
@@ -2,39 +2,63 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 class SGD:
-    def __init__(self, calc_grad, lr=0.01, stop_condition=0.0001):
-        self.calc_grad = calc_grad
-        self.lr = lr # learning rate
+    def __init__(self, loss_fn, lr=0.01, stop_condition=0.0001, batch_size=100):
+        self.loss_fn = loss_fn
+        self.learning_rate = lr
         self.stop_condition = stop_condition
+        self.batch_size = batch_size
 
     def run(self, X, Y, Theta):
-        loss_grads = []
-        grad = self.calc_grad(X, Y, Theta)
-        while np.linalg.norm(grad) > self.stop_condition:
-            loss_grads.append(np.linalg.norm(grad))
-            # print(f'{Theta.shape=}, {(self.lr * grad).shape=}')
-            Theta -= self.lr * grad
-            grad = self.calc_grad(X, Y, Theta)
-        return Theta, loss_grads
+        losses = []
+        grad_tot = np.inf
+        # each iteration is an epoch
+        while len(losses) <= 5 or abs(losses[-1] - losses[-5]) > self.stop_condition:
+            loss_tot = 0
+            grad_tot = 0
+            # each iteration is a batch
+            for i in range(0, X.shape[0], self.batch_size):
+                X_batch = X[i:i+self.batch_size]
+                Y_batch = Y[i:i+self.batch_size]
+                loss_tot += self.loss_fn(X_batch, Y_batch, Theta)
+                grad = self.loss_fn.calc_grad(X_batch, Y_batch, Theta)
+                Theta_change = -self.learning_rate * grad
+                grad_tot += np.linalg.norm(Theta_change)
+                Theta += Theta_change
+            losses.append(loss_tot / X.shape[0])
+            if len(losses) % 200 == 0:
+                print(f'Loss: {losses[-1]}')
+        return Theta, losses
 
-class CalcGrad:
+class LossFunction:
     def __init__(self):
         pass
 
     def __call__(self, X, Y, Theta):
         pass
 
-class LeastSquaresCG(CalcGrad):
+    def calc_grad(self, X, Y, Theta):
+        pass
+
+    def get_theta_shape(self, X, Y):
+        pass
+
+class LinearLeastSquaresLoss(LossFunction):
     def __init__(self):
         super().__init__()
 
     def __call__(self, X, Y, Theta):
-        return X.T @ (X @ Theta - Y)
+        # return 0.5 * sum(np.linalg.norm(x @ Theta - y)**2 for x, y in zip(X, Y)) / X.shape[0]
+        return 0.5 * np.linalg.norm(X @ Theta - Y) ** 2 / X.shape[0] #? is it ok to divide by X.shape[0]?
+
+    def calc_grad(self, X, Y, Theta):
+        # return sum(x.T @ (x @ Theta - y) for x, y in zip(X, Y)) / X.shape[0]
+        return X.T @ (X @ Theta - Y) / X.shape[0] #? is it ok to divide by X.shape[0]?
+    
+    def get_theta_shape(self, X, Y):
+        return (X.shape[1], Y.shape[1])
 
 
 import scipy.io as sio
-
-BATCH_SIZE = 100
 
 # open .mat file
 mat_contents = sio.loadmat('GMMData.mat')
@@ -44,17 +68,19 @@ Y = mat_contents['Yt'].T
 
 # Shuffle the data
 indices = np.random.permutation(X.shape[0])
-X = X[indices][:BATCH_SIZE]
-Y = Y[indices][:BATCH_SIZE]
+X = X[indices]
+Y = Y[indices]
 
 # X: (batch_size, n), Y: (batch_size, m), Theta: (n, m)
-Theta = np.zeros((X.shape[1], Y.shape[1]))
+loss_fn = LinearLeastSquaresLoss()
+Theta = np.zeros(loss_fn.get_theta_shape(X, Y))
 print(f'{X.shape=}, {Y.shape=}, {Theta.shape=}')
 
 # train
-my_sgd = SGD(LeastSquaresCG(), lr=0.0001, stop_condition=0.0001)
-theta, loss_grads = my_sgd.run(X, Y, Theta)
-print(theta)
-print(f'{theta.shape=}')
+my_sgd = SGD(loss_fn, lr=0.0001, stop_condition=0.000000001, batch_size=1000)
+Theta, loss_grads = my_sgd.run(X, Y, Theta)
+print(Theta)
+print(f'{Theta.shape=}')
+print(f'last loss: {loss_grads[-1]}')
 plt.plot(loss_grads)
 plt.show()
